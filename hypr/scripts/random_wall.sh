92fe4ee49ba1148a97ad310a4976434dd1bb7b74
ArixElo <patroxgamer@gmail.com>
2026-01-25 00:00:32 +0100

    config: hypr: remove unused random wall script
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- hypr/scripts/random_wall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
-
-# Get a random wallpaper that is not the current one
-WALLPAPER=$(find "~/.config/nazuna-walls/" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-
-# Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
