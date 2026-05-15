47028f92b73e310f7647e649235651baa621463c
ArixElo <patroxgamer@gmail.com>
2026-05-15 20:26:14 +0200

    hypr: lookandfeel: fixup animations once for all
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- hypr/env.lua
+++ hypr/env.lua
@@ -1,5 +1,5 @@
 -- Force all apps to use Wayland
-hl.env("GDK_BACKEND","wayland","x11")
+hl.env("GDK_BACKEND", "wayland","x11,*")
 hl.env("QT_QPA_PLATFORM","wayland;xcb")
 hl.env("STYLE_OVERRIDE","kvantum")
 hl.env("SDL_VIDEODRIVER","wayland")
--- hypr/lookandfeel.lua
+++ hypr/lookandfeel.lua
@@ -32,19 +32,20 @@ hl.config({
     },
 })
 
--- Bezier beziers
-hl.bezier("easeOutQuint",    0.23, 1,    0.32, 1)
-hl.bezier("easeInOutCubic",  0.65, 0.05, 0.36, 1)
-hl.bezier("linear",          0,    0,    1,    1)
-hl.bezier("almostLinear",    0.5,  0.5,  0.75, 1.0)
-hl.bezier("quick",           0.15, 0,    0.1,  1)
+-- Curves
+hl.curve("easeOutQuint",    { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
+hl.curve("easeInOutCubic",  { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
+hl.curve("linear",          { type = "bezier", points = { {0, 0},       {1, 1}       } })
+hl.curve("almostLinear",    { type = "bezier", points = { {0.5, 0.5},   {0.75, 1.0}  } })
+hl.curve("quick",           { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
 -- Animations
 hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
 hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
 hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, bezier = "easeOutQuint" })
 hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  bezier = "easeOutQuint", style = "popin 87%" })
 hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
-hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier
+ = "almostLinear" })
 hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
 hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
 hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
@@ -56,7 +57,6 @@ hl.animation({ leaf = "workspaces",    enabled = false })
 
 hl.config({
     dwindle = {
-        pseudotile = true,
         preserve_split = true,
         force_split = 2,
     },
