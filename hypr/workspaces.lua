d2c5e2b4f2fbc150c8bc3269426eae5e74c6c1d5
ArixElo <patroxgamer@gmail.com>
2026-05-15 20:55:59 +0200

    hypr: workspaces: use correct syntax
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- hypr/workspaces.lua
+++ hypr/workspaces.lua
@@ -1,2 +1,2 @@
-hl.workspace("1", { monitor = "HDMI-A-1", default = true })
-hl.workspace("2", { monitor = "DP-3",     default = true })
+hl.workspace_rule({workspace = "1", monitor = "HDMI-A-1", default = true })
+hl.workspace_rule({workspace = "2", monitor = "DP-3",     default = true })
