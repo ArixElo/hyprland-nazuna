d67ac8d2bc3602742a49c4927e375536aff16202
ArixElo <patroxgamer@gmail.com>
2026-05-15 22:41:42 +0200

    waybar & hypr: Add temporary fix for workspaces and add temporary scripts to open floated apps with Lua dispatchers
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

---
+++ hypr/scripts/battop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+hyprctl dispatch 'hl.dsp.exec_cmd("kitty battop", { float = true })'
---
+++ hypr/scripts/s-tui.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+hyprctl dispatch 'hl.dsp.exec_cmd("kitty s-tui", { float = true })'
