26688faff39bd36f32905ad1f3044c4c10168937
ArixElo <patroxgamer@gmail.com>
2026-08-17 18:53:57 +0200

    config: hypr: set the cursor size to 24
    for some fucking reason this thing got smaller as fuck
    
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -7,7 +7,7 @@
    hl.exec_cmd("/usr/libexec/hyprpolkitagent")
    hl.exec_cmd("gentoo-pipewire-launcher")
    hl.exec_cmd("~/.cargo/bin/wpaperd")
-   hl.exec_cmd("hyprctl setcursor Adwaita 15")
+   hl.exec_cmd("hyprctl setcursor Adwaita 24")
    hl.exec_cmd("linux-wallpaperengine --screen-root DP-2 --bg 3568585209 -f 75  --scaling fill --clamp border")
    hl.exec_cmd("linux-wallpaperengine --screen-root HDMI-A-1 --bg 3568585209 -f 60  --scaling fill --clamp border")
  end)
